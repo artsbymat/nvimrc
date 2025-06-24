3948fedeaf7008380694652410fa5b96ee3ca451
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-06-24 18:46:11 +0700

    chore: remove options to wakatime plugin

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,16 +1,4 @@
 return {
 	"wakatime/vim-wakatime",
 	lazy = false,
-	opts = {
-		ignore = {
-			"COMMIT_EDITMSG$",
-			"PULLREQ_EDITMSG$",
-			"MERGE_MSG$",
-			"TAG_EDITMSG$",
-			"%.tmp$",
-			"/tmp/",
-			"%.log",
-			"brain-hub/",
-		},
-	},
 }
