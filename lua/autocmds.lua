fecb16e55517cc4cf227b1d85fdd5e831ee2f47d
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-06-25 20:46:12 +0700

    chore: add toggle case function

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- Function to toggle case of selected text
+vim.cmd([[
+function! TwiddleCase(str)
+  if a:str ==# toupper(a:str)
+    let result = tolower(a:str)
+  elseif a:str ==# tolower(a:str)
+    let result = substitute(a:str,'\(\<\w\+\>\)', '\u\1', 'g')
+  else
+    let result = toupper(a:str)
+  endif
+  return result
+endfunction
+
+vnoremap ~ y:call setreg('', TwiddleCase(@"), getregtype(''))<CR>gv""Pgv
+]])
